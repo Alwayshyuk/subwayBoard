5f6141f8607da2744067a83248abcc6b0234131a
han93 <never5686@naver.com>
2024-06-19 23:53:18 +0900

    Initial commit

---
+++ src/main/java/me/sanghyuk/subwayboard/SubwayBoardApplication.java
@@ -0,0 +1,13 @@
+package me.sanghyuk.subwayboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubwayBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubwayBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/sanghyuk/subwayboard/SubwayBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sanghyuk.subwayboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubwayBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
